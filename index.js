ff31872ed741e73a7937a972c37a4d7921677b6d
Sukran <sukran.akyuz.93@gmail.com>
2023-04-01 11:49:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+let currentUser = "Şükran"
+
+let welcomeMessage = "Welcome to Flatbook, Şükran!"
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ŞÜKRAN!"
+
+let shortGreeting = "Welcome, Ş!"
